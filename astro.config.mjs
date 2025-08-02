6eae9f8a12f8a6168575e57cc15e58a691d542cb
Zeus Rodriguez <zlrm2808@gmail.com>
2025-08-02 16:21:20 -0400

    ultimos cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
 
 export default defineConfig({
   output: "server",
+  adapter: node({
+    mode: 'standalone'
+  }),
   vite: {
     plugins: [tailwindcss()]
   }
